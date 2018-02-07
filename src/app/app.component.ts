c2d21c6baa62bf9b400dcf0e0d960794aab0f18f
Michał Gołębiowski-Owczarek <michal.golebiowski-owczarek@yougov.com>
2018-02-07 20:37:36 +0100

    Delay Router initialNavigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'yg-root',
@@ -7,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private router: Router,
+  ) {}
 
 
   ngOnInit() {
     console.log('AppComponent#ngOnInit');
+    this.router.initialNavigation();
   }
 }
